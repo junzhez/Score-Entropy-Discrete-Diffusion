2c462dcc47f8b893efa768c55d8da7ef29e1d759
Junzhe Zhang <junzhez@junzhez.com>
2025-07-25 16:33:02 -0400

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -172,9 +172,10 @@ def get_pc_sampler(graph, noise, batch_dims, predictor, steps, denoise=True, eps
                 x = predictor.update_fn(sampling_score_fn, x, t, dt)
         
         loss_fun = losses.get_loss_fn(noise, graph, train=False)
-        alpha = torch.clamp(torch.exp(loss_fun(model, x)) / torch.exp(loss_fun(model, x_prev)), max=1.0)
+        alpha = torch.clamp(torch.exp(loss_fun(model, x)) / torch.exp(loss_fun(model, x_prev)), max=1.0).item()
 
-        u = torch.randn(1, device=device)
+        print(alpha)
+        u = np.randn()
 
         if u < alpha:
             x = x_prev
