cbaa5e8b7535fb0f0f0b858a21995676e3cbde94
Junzhe Zhang <junzhez@junzhez.com>
2025-07-25 17:02:23 -0400

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -172,14 +172,14 @@ def get_pc_sampler(graph, noise, batch_dims, predictor, steps, denoise=True, eps
         
         x_prev = x.clone()
 
-        loss_fun = losses.get_loss_fn(noise, graph)
+        loss_fun = losses.get_loss_fn(noise, graph, train=False)
 
         alpha1 = loss_fun(model, x)
         alpha2 = loss_fun(model, x_prev)
         alpha = torch.clamp(torch.exp(alpha1-alpha2), max=1)
 
         print(alpha1, alpha2, alpha)
-
+        
         if threshold:
             u = torch.randn(1, device=device)
 
